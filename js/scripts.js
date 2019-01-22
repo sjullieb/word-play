7dbb2ee6214af49c52f3591d0f951182c727b679
Jared Hanson and Yulia Shidlovskaya <yulia@>
2019-01-22 13:51:55 -0800

    replaced map to forEach to ret gid of spaces that were counted as elements for final array

--- js/scripts.js
+++ js/scripts.js
@@ -2,72 +2,22 @@ $(document).ready(function() {
 
 
    $("#run").submit(function(event){
-     var input = $("#input").val();
 
-     var arrays = input.split(' ');
-     alert('length ' + arrays.length);
-     var alterArray = arrays.map(function(array) {
-      alert("1st " + array + ' ' + array.length);
-      if (array.length >= 3 && ($.trim(array) !== '')) {
-         alert("2nd " + array + ' ' + array.length);
-         return array;
+    var words = $("#input").val().split(" ");
+
+alert(words.length);
+    var newArray = [];
+    words.forEach(function(word){
+      if (word.length >= 3){
+        newArray.push(word);
       }
-     });
+    });
 
-     alert(alterArray.length);
-     alert(alterArray.reverse().join(' '));
-
-     event.preventDefault();
-   });
-
-
-
- });
+alert(newArray.length);
+    alert(newArray.reverse().join(" "));
+    event.preventDefault();
+  });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //
-  //   var words = $("#input").val().split(" ");
-  //
-  //   var newArray = words.map(function(word){
-  //     if (word.length >= 3){
-  //       return word;
-  //     }
-  //   });
-  //
-  //   alert(newArray.reverse().join(" "));
-//     event.preventDefault();
-//   });
-//
-//
-//
-// });
+});
